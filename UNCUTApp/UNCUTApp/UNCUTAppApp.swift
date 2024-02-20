7e6342cc914114b22ceeea8a4704789f672e7da0
lailaeliotti1 <93938274+lailaeliotti1@users.noreply.github.com>
2024-02-20 13:49:14 -0600

    Xcode creation -LE

---
+++ UNCUTApp/UNCUTApp/UNCUTAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UNCUTAppApp.swift
+//  UNCUTApp
+//
+//  Created by Laila Eliotti on 2/20/24.
+//
+
+import SwiftUI
+
+@main
+struct UNCUTAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
