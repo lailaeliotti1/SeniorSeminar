c94406db60acec8aff94ba23225d23f67dec3a94
lailaeliotti1 <93938274+lailaeliotti1@users.noreply.github.com>
2024-03-28 13:29:52 -0500

    new update and reset function commands

--- UNCUTApp/UNCUTApp/Authentication/SettingsView.swift
+++ UNCUTApp/UNCUTApp/Authentication/SettingsView.swift
@@ -13,11 +13,30 @@ final class SettingsViewModel: ObservableObject {
     func logOut() throws {
         try AuthManager.shared.signOut()
     }
+    
+    func resetPassword() async throws {
+        let authUser = try AuthManager.shared.getAuthenticatedUser()
+        guard let email = authUser.email else {
+            throw URLError(.fileDoesNotExist)
+        }
+        try await AuthManager.shared.resetPassword(email: email)
+    }
+    
+    func updateEmail() async {
+        let email = "hello123@testing.com"
+        try? await AuthManager.shared.updateEmail(email: email)
+    }
+    
+    func updatePassword() async {
+        let password = "hello123"
+        try? await AuthManager.shared.updatePassword(password: password)
+    }
 }
 struct SettingsView: View {
     @StateObject private var viewModel = SettingsViewModel()
     @Binding var showSignInView: Bool
-    
+
+   
     var body: some View {
         List {
             Button("Log out"){
@@ -31,6 +50,43 @@ struct SettingsView: View {
                     }
                 }
             }
+            
+            Button("Reset Password"){
+                Task {
+                    do {
+                        try await viewModel.resetPassword()
+                        print("PASSWORD RESET")
+                    }
+                    catch {
+                        print(error)
+                    }
+                }
+            }
+            
+            Button("Update Password"){
+                Task {
+                    do {
+                        try await viewModel.updatePassword()
+                        print("PASSWORD UPDATED")
+                    }
+                    catch {
+                        print(error)
+                    }
+                }
+            }
+            
+            Button("Update Email"){
+                Task {
+                    do {
+                        try await viewModel.updatePassword()
+                        print("EMAIL UPDATED")
+                    }
+                    catch {
+                        print(error)
+                    }
+                }
+            }
+            
         }
         .navigationBarTitle("Settings")
     }
